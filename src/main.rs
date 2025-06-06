62d52e0c7157b5a0ff8f1cd0fafe20ecde3076e2
Frederick Price <rprice@pricemail.ca>
2025-06-06 15:01:04 -0400

    Make things a bit more idiomatic

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-//use rust_forth_tokenizer::ForthToken;
 use rust_forth_tokenizer::ForthTokenizer;
 use std::env;
 use std::fs;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: {} <file> [file2 ...]", args[0]);
+        std::process::exit(1);
+    }
+
     for file in &args[1..] {
-        let file_string = fs::read_to_string(file).expect("Failure opening file");
+        let file_string = fs::read_to_string(file)
+            .unwrap_or_else(|e| panic!("Failed to open file '{}': {}", file, e));
 
         for token in ForthTokenizer::new(&file_string) {
             println!("Token is: {:?}", token);
